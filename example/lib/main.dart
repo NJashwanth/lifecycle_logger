cbb855f1ee9c1a5324f8cbc45f67f3ebfe336465
JASHWANTH NEELA <jashwanthn066@gmail.com>
2026-02-22 19:06:56 -0500

    Implement interactive example app

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,20 +1,64 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 import 'package:lifecycle_logger/lifecycle_logger.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  LifecycleLogger.attach();
-  runApp(const _ExampleApp());
+  LifecycleLogger.attach(
+    onResume: () => debugPrint('[Lifecycle] Example callback onResume'),
+    onPause: () => debugPrint('[Lifecycle] Example callback onPause'),
+    onInactive: () => debugPrint('[Lifecycle] Example callback onInactive'),
+    onDetached: () => debugPrint('[Lifecycle] Example callback onDetached'),
+  );
+  runApp(const ExampleApp());
 }
 
-class _ExampleApp extends StatelessWidget {
-  const _ExampleApp();
+class ExampleApp extends StatelessWidget {
+  const ExampleApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Directionality(
-      textDirection: TextDirection.ltr,
-      child: _LifecycleProbe(),
+    return const MaterialApp(
+      home: _ExampleHome(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class _ExampleHome extends StatefulWidget {
+  const _ExampleHome();
+
+  @override
+  State<_ExampleHome> createState() => _ExampleHomeState();
+}
+
+class _ExampleHomeState extends State<_ExampleHome> {
+  var _showProbe = true;
+
+  void _toggleProbe() {
+    setState(() {
+      _showProbe = !_showProbe;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('lifecycle_logger example')),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const Text('Watch debug console for lifecycle logs.'),
+            const SizedBox(height: 12),
+            ElevatedButton(
+              onPressed: _toggleProbe,
+              child: Text(_showProbe ? 'Dispose probe' : 'Create probe'),
+            ),
+            const SizedBox(height: 12),
+            if (_showProbe) const _LifecycleProbe(),
+          ],
+        ),
+      ),
     );
   }
 }
@@ -30,16 +74,16 @@ class _LifecycleProbeState extends State<_LifecycleProbe>
     with LifecycleAware<_LifecycleProbe> {
   @override
   void onInit() {
-    debugPrint('[Lifecycle] _LifecycleProbe onInit hook');
+    debugPrint('[Lifecycle] _LifecycleProbe onInit hook fired');
   }
 
   @override
   void onDispose() {
-    debugPrint('[Lifecycle] _LifecycleProbe onDispose hook');
+    debugPrint('[Lifecycle] _LifecycleProbe onDispose hook fired');
   }
 
   @override
   Widget build(BuildContext context) {
-    return const SizedBox.shrink();
+    return const Text('Probe mounted (uses LifecycleAware)');
   }
 }
